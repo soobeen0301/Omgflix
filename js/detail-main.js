ebd65b14f802bc7c304515556cd4a06795147e3b
lys <projectys29@gmail.com>
2024-05-01 18:30:57 +0900

    상세 페이지 상단 이동 버튼

--- js/detail-main.js
+++ js/detail-main.js
@@ -1,6 +1,9 @@
 import { fetchMovies } from './detail-api.js';
 import { loading } from './detail-loding.js';
+import { moveTop } from './move-top-detail.js';
 
 fetchMovies();
 
 loading();
+
+moveTop();
