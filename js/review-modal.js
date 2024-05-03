5e63383fbf75f9843ce9278f466240a5032b6574
lys <projectys29@gmail.com>
2024-05-03 15:40:33 +0900

    모달창 수정

---
+++ js/review-modal.js
@@ -0,0 +1,18 @@
+const authorInput = document.getElementById('authorInput');
+const passwordInput = document.getElementById('passwordInput');
+const reviewInput = document.getElementById('reviewInput');
+
+export function openModal() {
+    document.getElementById('popOpen').addEventListener('click', function () {
+        document.querySelector('.popWrap').style.display = 'flex';
+    });
+}
+
+export function closeModal() {
+    document.getElementById('popClose').addEventListener('click', function () {
+        document.querySelector('.popWrap').style.display = 'none';
+        authorInput.value = '';
+        passwordInput.value = '';
+        reviewInput.value = '';
+    });
+}
