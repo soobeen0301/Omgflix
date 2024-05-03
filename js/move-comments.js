e7f553de291654b9b5dedc51d59b7584f941fd71
kimmangyu <vlsual0917@gmail.com>
2024-05-03 11:11:20 +0900

    add : 리뷰 CRUD 영화마다 기능 동작

--- js/move-comments.js
+++ js/move-comments.js
@@ -44,19 +44,27 @@ export const initializeEventHandlers = () =>{
   });
 }
 
-// 페이지 로드
+// 페이지 로드 시 특정 영화의 리뷰만 로드
 const loadReviews = () => {
   const reviews = JSON.parse(localStorage.getItem('reviews')) || [];
-  reviews.forEach((review, index) => {
+  const movieId = getQueryStringValue('id');
+  // 해당 영화 ID에 맞는 리뷰만 필터링
+  const filteredReviews = reviews.filter(review => review.movieId === movieId);
+  console.log(filteredReviews)
+  filteredReviews.forEach((review, index) => {
     displayReview(review, index);
   });
 }
+
 
 // 리뷰 생성
 const addReview = (name, review, password) => {
   const reviews = JSON.parse(localStorage.getItem('reviews')) || [];
   const newIndex = reviews.length;
-  const reviewObj = { name, review, password };
+  const movieId = getQueryStringValue('id');
+  // 리뷰 객체에 영화 ID 추가
+  const reviewObj = { name, review, password, movieId };
+  console.log(reviewObj)
   reviews.push(reviewObj);
   localStorage.setItem('reviews', JSON.stringify(reviews));
   displayReview(reviewObj, newIndex);
@@ -82,8 +90,9 @@ const displayReview = (review, index) => {
 // 리뷰 수정
 const editReview = (reviewDiv, index) => {
   const reviews = JSON.parse(localStorage.getItem('reviews'));
-  if (!reviews || index < 0 || index >= reviews.length) {
-    console.error('유효하지 않은 인덱스입니다.');
+  const movieId = getQueryStringValue('id');
+  if (!reviews || index < 0 || index >= reviews.length || reviews[index].movieId !== movieId) {
+    console.error('유효하지 않은 인덱스이거나 영화 ID가 일치하지 않습니다.');
     return;
   }
   const newPassword = prompt('패스워드를 입력하세요:');
@@ -100,7 +109,10 @@ const editReview = (reviewDiv, index) => {
 // 리뷰 삭제
 const deleteReview = (reviewDiv, index) => {
   const reviews = JSON.parse(localStorage.getItem('reviews'));
-  if (reviews && index >= 0 && index < reviews.length) {
+  const movieId = getQueryStringValue('id');
+  console.log(reviews)
+  console.log(movieId)
+  if (reviews && index >= 0 && index < reviews.length && reviews[index].movieId === movieId) {
     const password = prompt('패스워드를 입력하세요:');
     if (password && verifyPassword(index, password)) {
       reviews.splice(index, 1);
@@ -110,7 +122,7 @@ const deleteReview = (reviewDiv, index) => {
       alert('올바른 패스워드를 입력하세요!');
     }
   } else {
-    console.error('삭제할 리뷰가 없습니다.');
+    console.error('삭제할 리뷰가 없거나 영화 ID가 일치하지 않습니다.');
   }
 }
 
@@ -121,4 +133,10 @@ const verifyPassword = (index, password) => {
     return reviews[index].password === password;
   }
   return false;
+}
+
+// URL에서 쿼리 스트링을 파싱하여 특정 키의 값을 반환하는 함수
+const getQueryStringValue = (key) => {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(key);
 }
