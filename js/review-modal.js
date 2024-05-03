613709b338944ad8d15948e25b6c5658b2e4ca5f
KR-EGOIST <jhso5041@naver.com>
2024-05-03 20:45:44 +0900

    ✨ update : 모달창 오픈시 스크롤 방지

--- js/review-modal.js
+++ js/review-modal.js
@@ -1,18 +1,21 @@
 const authorInput = document.getElementById('authorInput');
 const passwordInput = document.getElementById('passwordInput');
 const reviewInput = document.getElementById('reviewInput');
+const html = document.querySelector('html');
 
 export function openModal() {
-    document.getElementById('popOpen').addEventListener('click', function () {
-        document.querySelector('.popWrap').style.display = 'flex';
-    });
+  document.getElementById('popOpen').addEventListener('click', function () {
+    document.querySelector('.popWrap').style.display = 'flex';
+    html.style.overflow = 'hidden';
+  });
 }
 
 export function closeModal() {
-    document.getElementById('popClose').addEventListener('click', function () {
-        document.querySelector('.popWrap').style.display = 'none';
-        authorInput.value = '';
-        passwordInput.value = '';
-        reviewInput.value = '';
-    });
+  document.getElementById('popClose').addEventListener('click', function () {
+    document.querySelector('.popWrap').style.display = 'none';
+    html.style.overflow = 'auto';
+    authorInput.value = '';
+    passwordInput.value = '';
+    reviewInput.value = '';
+  });
 }
