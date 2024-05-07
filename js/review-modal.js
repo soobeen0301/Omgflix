5898f63dbb9ba061b01b6be2f2ecf672225a3688
KR-EGOIST <jhso5041@naver.com>
2024-05-07 14:21:12 +0900

    🐛 bugfix : 모달창 등록버튼 누를 시 화면 멈춤 버그 수정

--- js/review-modal.js
+++ js/review-modal.js
@@ -6,14 +6,12 @@ const html = document.querySelector('html');
 export function openModal() {
   document.getElementById('popOpen').addEventListener('click', function () {
     document.querySelector('.popWrap').style.display = 'flex';
-    html.style.overflow = 'hidden';
   });
 }
 
 export function closeModal() {
   document.getElementById('popClose').addEventListener('click', function () {
     document.querySelector('.popWrap').style.display = 'none';
-    html.style.overflow = 'auto';
     authorInput.value = '';
     passwordInput.value = '';
     reviewInput.value = '';
